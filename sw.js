f604391687d3510824587a79c0f901c3bec1aecd
Build Server <git@bitcoinpy.io>
2018-12-23 18:05:09 +1000

    Updates

--- sw.js
+++ sw.js
@@ -32,14 +32,14 @@ self.__precacheManifest = [
     "url": "app.8b1cc9920e75fff049f7.css"
   },
   {
-    "url": "app-e4e2d150c16eb3b249a6.js"
+    "url": "app-62ff65486aa68df13be3.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-76d1630f66cfbae8d146.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "385b4f5f8608a21c7d88ec740aaff86d"
+    "revision": "d26c500f4b0dc1f3eeba9ef6d6748739"
   },
   {
     "url": "component---src-pages-404-js-b81fbe590491a4ef7bae.js"
